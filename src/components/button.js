1d75e01821addfe36446541f11fd13d44bd6e62e
Fröhlich A <froald14@new.edvhtl.at>
2018-08-04 21:22:53 +0200

    Add content to index page

---
+++ src/components/button.js
@@ -0,0 +1,26 @@
+import glamorous from 'glamorous';
+import Link from "gatsby-link";
+
+/**
+ * A button on the navbar.
+ */
+export default glamorous(Link)({
+  appearance: 'button',
+  position: 'relative',
+  height: '40px',
+  backgroundColor: 'inherit',
+  border: '2px solid #e9f7fe',
+  boxShadow: '2px 2px white',
+  fontSize: '14px',
+  color: 'white',
+  textTransform: 'uppercase',
+  cursor: 'pointer',
+  textDecoration: 'none',
+  padding: '8px 10px 8px 10px',
+  marginRight: 15,
+  ':hover': {
+    boxShadow: '0px 0px',
+    left: 2,
+    top: 2
+  }
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Blockquote } from 'glamorous';
 import Headline from '../components/headline';
 import SubHeadline from '../components/subheadline';
+import Button from '../components/button';
 
 export default () => (
   <React.Fragment>
     <Headline>One Team. One Goal.</Headline>
     <SubHeadline>Free Software. Free as in Speech, Free as in Beer.</SubHeadline>
+    <Blockquote marginTop='3.2rem' borderLeft='2px solid white' paddingLeft='20px' marginLeft='0px'>
+      Once upon a time, there were two programmers.<br />
+      After the last bug in this legacy Magento project was fixed, they grew bored.<br />
+      And so, they started their own free, open-source, projects.
+    </Blockquote>
+    <Button to="/projects">Look at the results</Button>
   </React.Fragment>
 );
