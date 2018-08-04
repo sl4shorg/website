48147c565228093d8c743a282fdf91dfa60abae5
Fröhlich A <froald14@new.edvhtl.at>
2018-08-04 11:44:59 +0200

    Change typography config to include google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,17 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: `src/utils/config.js`
+        pathToConfigModule: 'src/utils/config.js'
       }
     },
-    `gatsby-plugin-react-next`
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['Source Sans Pro', 'Source Code Pro']
+      }
+    },
+    'gatsby-plugin-react-next'
   ]
 };
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,10 @@
 import Typography from 'typography';
 import './global.css';
 
-const typography = new Typography({ baseFontSize: '18px' });
+const typography = new Typography({
+  baseFontSize: '18px',
+  headerFontFamily: ['Source Code Pro', 'sans-serif'],
+  bodyFontFamily: ['Source Sans Pro', 'sans-serif']
+});
 
 export default typography;
